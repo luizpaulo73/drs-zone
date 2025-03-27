3886f1464a318856068a123ac45daf56e542d425
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-03-27 10:14:07 -0300

    Componente de eventos proximos adicionado e responsivo

--- drs-zone/src/app/page.tsx
+++ drs-zone/src/app/page.tsx
@@ -1,11 +1,17 @@
 import Hero from "@/components/Hero/Hero";
+import ProximosEventos from "@/components/ProximosEventos/ProximosEventos";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
     <main className="p-2 lg:p-4 flex flex-col lg:flex-row">
-      <Sidebar/>  
-      <Hero />
+      <div className="flex flex-col lg:flex-row w-full">
+        <Sidebar/>
+        <div className="w-full">
+          <Hero />
+          <ProximosEventos />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ drs-zone/src/components/ProximosEventos/ProximosEventos.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
+
+export default async function ProximosEventos() {
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const response = await fetch(`http://api.jolpi.ca/ergast/f1/${year}/driverstandings/`);
+    const response2 = await fetch(`https://api.jolpi.ca/ergast/f1/${year}/constructorstandings/?format=json`);
+  
+  if (!response.ok || !response2.ok) {
+    throw new Error("Erro ao carregar dados");
+  }
+
+    const pilotoResponse = await response.json();
+    const equipeResponse = await response2.json();
+
+    const piloto = pilotoResponse.MRData.StandingsTable.StandingsLists[0].DriverStandings[0];
+    const equipe = equipeResponse.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0];
+    const pilotosDaEquipe = pilotoResponse.MRData.StandingsTable.StandingsLists[0].DriverStandings
+  .filter(piloto => piloto.Constructors.some(constructor => constructor.constructorId === equipe.Constructor.constructorId))
+  .map(piloto => `${piloto.Driver.familyName}`)
+  .slice(0, 2);
+
+
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-3 gap-3 mt-5">
+      <div className="border border-zinc-700 w-full sm:w-full p-4 flex flex-row justify-between items-center gap-3">
+        <div>
+          <h2 className="text-gray text-sm">Próxima Corrida</h2>
+          <p className="text-white font-bold sm:text-2xl">Grande Prêmio de Mônaco</p>
+        </div>
+        <Link href={"/"} className="text-white p-2 hover:text-zinc-400 duration-300">
+          <ExternalLink />
+        </Link>
+      </div>
+      <div className="border border-zinc-700 w-full sm:w-full p-4 flex flex-row justify-between items-center gap-3">
+        <div>
+          <h2 className="text-gray text-sm">Líder de Pilotos</h2>
+          <div className="flex items-center gap-2">
+            <p className="text-white font-bold sm:text-2xl">{piloto.Driver.givenName + " " + piloto.Driver.familyName}</p>
+            <p className="text-white text-sm sm:text-base bg-blue-600 max-w-fit px-2 rounded-2xl">{piloto.points} pts</p>
+          </div>
+          
+          <p className="text-gray text-sm sm:text-base">{piloto.Constructors[0].name}</p>
+          
+        </div>
+        <Link href={"/"} className="text-white p-2 hover:text-zinc-400 duration-300">
+          <ExternalLink />
+        </Link>
+      </div>
+      <div className="border border-zinc-700 w-full sm:w-full p-4 flex flex-row justify-between items-center gap-3">
+        <div>
+          <h2 className="text-gray text-sm">Líder de Construtores</h2>  
+          <div className="flex items-center gap-2">
+            <p className="text-white font-bold sm:text-2xl">{equipe.Constructor.name}</p>
+            <p className="text-white text-sm sm:text-base bg-blue-600 max-w-fit px-2 rounded-2xl">{equipe.points} pts</p>
+          </div>
+          <p className="text-gray text-sm sm:text-base">{pilotosDaEquipe[0] + " e " + pilotosDaEquipe[1]}</p>
+        </div>
+        <Link href={"/"} className="text-white p-2 hover:text-zinc-400 duration-300">
+          <ExternalLink />
+        </Link>
+      </div>
+    </section>
+  );
+}
